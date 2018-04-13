f7b5f447ba6f366d942a5582e94bfad69f3f5a6f
Tom Zdulski <tomzdulski@gmail.com>
2018-04-13 17:13:20 +0200

    Commit 2 - Adding other language as default language

--- fuel/application/config/MY_fuel.php
+++ fuel/application/config/MY_fuel.php
@@ -1,9 +1,9 @@
 <?php
 /*
-|---------------------------------------------------------------------------------------------------
-| IMPORTANT: for a complete list of fuel configurations, go to the modules/fuel/config/fuel.php file
-|---------------------------------------------------------------------------------------------------
-*/
+  |---------------------------------------------------------------------------------------------------
+  | IMPORTANT: for a complete list of fuel configurations, go to the modules/fuel/config/fuel.php file
+  |---------------------------------------------------------------------------------------------------
+ */
 
 // path to the fuel admin from the web base directory... MUST HAVE TRAILING SLASH!
 $config['fuel_path'] = 'fuel/';
@@ -12,18 +12,16 @@ $config['fuel_path'] = 'fuel/';
 $config['site_name'] = 'My Website';
 
 // whether the admin backend is enabled or not
-$config['admin_enabled'] = TRUE; //FALSE;
-
+$config['admin_enabled']   = TRUE; //FALSE;
 // options are cms, views, and auto. 
 // cms pulls views and variables from the database,
 // views mode pulls views from the views folder and variables from the _variables folder,
 // and the auto option will first check the database for a page and if it doesn't exist or is 
 // not published, it will then check for the corresponding view file.
-$config['fuel_mode'] = 'auto'; //views';
-
+$config['fuel_mode']       = 'auto'; //views';
 // specifies which modules are allowed to be used in the fuel admin
 $config['modules_allowed'] = array(
-	'user_guide',
+    'user_guide',
 );
 
 // used for system emails
@@ -43,13 +41,13 @@ $config['max_page_params'] = 0;
 $config['auto_search_views'] = FALSE;
 
 // max upload files size for assets
-$config['assets_upload_max_size']	= 5000;
+$config['assets_upload_max_size'] = 5000;
 
 // max width for asset images being uploaded
-$config['assets_upload_max_width']  = 1024;
+$config['assets_upload_max_width'] = 1024;
 
 // max height for asset images being uploaded
-$config['assets_upload_max_height']  = 768;
+$config['assets_upload_max_height'] = 768;
 
 // text editor settings  (options are markitup or ckeditor)
 // markitup: allows you to visualize the code in its raw format - not wysiwyg (http://markitup.jaysalvat.com/)
@@ -61,35 +59,47 @@ $config['text_editor'] = 'markitup';
 $config['parser_engine'] = 'dwoo';
 
 // The directory to put the parsed compiled files
-$config['parser_compile_dir'] = APPPATH.'cache/dwoo/compiled/';
+$config['parser_compile_dir'] = APPPATH . 'cache/dwoo/compiled/';
 
 // The delimiters used by the parsing engine
 $config['parser_delimiters'] = array(
-				'tag_comment'   => array('{#', '#}'), // Twig only
-				'tag_block'     => array('{%', '%}'), // Twig only
-				'tag_variable'  => array('{', '}'), // Used by Twig, Dwoo and CI. Default for twig is '{{', '}}' and Dwoo is '{', '}'
-				'interpolation' => array('#{', '}'), // Twig only
-			);
+    'tag_comment'   => array('{#', '#}'), // Twig only
+    'tag_block'     => array('{%', '%}'), // Twig only
+    'tag_variable'  => array('{', '}'), // Used by Twig, Dwoo and CI. Default for twig is '{{', '}}' and Dwoo is '{', '}'
+    'interpolation' => array('#{', '}'), // Twig only
+);
 
 // Functions allowed by the parsing engine
 $config['parser_allowed_functions'] = array(
-	'strip_tags', 'date', 
-	'detect_lang','lang',
-	'js', 'css', 'swf', 'img_path', 'css_path', 'js_path', 'swf_path', 'pdf_path', 'media_path', 'cache_path', 'captcha_path', 'assets_path', // assets specific
-	'fuel_block', 'fuel_model', 'fuel_nav', 'fuel_edit', 'fuel_set_var', 'fuel_var', 'fuel_var_append', 'fuel_form', 'fuel_page', // FUEL specific
-	'quote', 'safe_mailto', // HTML/URL specific
-	'session_flashdata', 'session_userdata', // Session specific
-	'prep_url', 'site_url', 'show_404', 'redirect', 'uri_segment', 'auto_typography', 'current_url' // CI specific
+    'strip_tags', 'date',
+    'detect_lang', 'lang',
+    'js', 'css', 'swf', 'img_path', 'css_path', 'js_path', 'swf_path', 'pdf_path', 'media_path', 'cache_path', 'captcha_path', 'assets_path', // assets specific
+    'fuel_block', 'fuel_model', 'fuel_nav', 'fuel_edit', 'fuel_set_var', 'fuel_var', 'fuel_var_append', 'fuel_form', 'fuel_page', // FUEL specific
+    'quote', 'safe_mailto', // HTML/URL specific
+    'session_flashdata', 'session_userdata', // Session specific
+    'prep_url', 'site_url', 'show_404', 'redirect', 'uri_segment', 'auto_typography', 'current_url' // CI specific
 );
 
 /* Uncomment if you want to control FUEL settings in the CMS. Below are a couple examples of ones you can configure
-$config['settings'] = array();
-$config['settings']['site_name'] = array();
-if (!empty($config['modules_allowed']))
-{
-	$config['settings']['modules_allowed'] = array('type' => 'multi', 'options' => array_combine($config['modules_allowed'], $config['modules_allowed']));
-}
-*/
+  $config['settings'] = array();
+  $config['settings']['site_name'] = array();
+  if (!empty($config['modules_allowed']))
+  {
+  $config['settings']['modules_allowed'] = array('type' => 'multi', 'options' => array_combine($config['modules_allowed'], $config['modules_allowed']));
+  }
+ */
+
+$config['settings']              = array();
+$config['settings']['languages'] = array(
+    'type'                  => 'keyval',
+    'fields'                => array(
+        'key'   => array('ignore_representative' => TRUE),
+        'label' => array('ignore_representative' => TRUE),
+    ),
+    'class'                 => 'repeatable',
+    'repeatable'            => TRUE,
+    'ignore_representative' => TRUE
+);
 
 
 
